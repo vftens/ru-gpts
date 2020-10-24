191946bb4c472bd7f4d69577f9be3d722c6720fd
vftens <aico@ya.ru>
2020-10-24 19:43:26 +0300

    working Medium .bat
    working Medium .bat

---
+++ generate_ruGPT3Large.bat
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+
+python generate_transformers.py --model_type=gpt2 --model_name_or_path=sberbank-ai/rugpt3large_based_on_gpt2 --k=10 --p=0.9 
+rem --length=100
---
+++ generate_ruGPT3Medium_2.bat
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+python generate_transformers.py --model_type=gpt2 --model_name_or_path=sberbank-ai/rugpt3medium_based_on_gpt2 --k=50 --p=0.9 --length=1536 --seed=1453 >> output.txt
